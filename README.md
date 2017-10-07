5180c8182379abd59d2d4ab2a7ff7572cbe01d5d
fafaz <fafazlab@gmail.com>
2017-10-07 16:52:50 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# fafaz-Popover
+fafaz-Popover is a lightweight popover plugin with no dependencies
+
+Example Page: [https://fafaz.github.io/fafaz-popover/demo/demo.html](https://fafaz.github.io/fafaz-popover/demo/demo.html)
+
+
+</br>
+
+## Instructions
+
+Install via add a css, javascript files from the [dist](dist) directory to your page.
+
+
+<br/>
+
+## Usage
+
+
+##### HTML
+
+```html
+<head>
+  ...
+  <script src="Popover.js"></script>
+  <link rel="stylesheet" href="Popover.css" />
+</head>
+
+<body>
+  <button class="trigger" data-layer-id="test-layer">
+    click here
+  </button>
+
+  <div id="test-layer" class="popover-layer">Your Contents</div>
+</body>
+```
+
+
+</br>
+
+##### Javascript
+
+```javascript
+var myPopover = new fafaz.Popover('.trigger', {
+  ...options
+});
+```
+
+
+<br/>
+
+## Options
+```javascript
+{
+  gutter: null,
+  overlapSelector: true,
+  callback: null
+}
+```
+
+
+<br/>
+
+## License
+
+MIT
---
+++ src/js/Popover.js
@@ -0,0 +1,107 @@
+require('../sass/Popover.scss');
+
+/**
+ * Copyright (c) fafazlab
+ * fafaz-popover projects are licensed under the MIT license
+ */
+
+export default class Popover {
+  constructor(trigger, options) {
+    if (!trigger) return;
+    this._trigger = document.querySelectorAll(trigger);
+    this._options = {
+      gutter: null,
+      overlapSelector: true,
+      callback: null
+    };
+
+    if (options) {
+      Object.assign(this._options, options);
+    }
+
+    document.addEventListener('click', (ev) => {
+      let layers = document.querySelectorAll('.popover-layer');
+      for (let i=0, c=layers.length; i<c; i++) {
+        if (ev.path.indexOf(layers[i]) !== -1) return;
+        layers[i].classList.remove('is-active');
+      }
+    }, false);
+
+    for (let i = 0, c = this._trigger.length; i < c; i++) {
+      this._trigger[i].addEventListener('click', (ev) => {
+        ev.stopPropagation();
+        let _this = ev.currentTarget || ev.target;
+        let id = _this.getAttribute('data-layer-id');
+        const layer = document.getElementById(id);
+
+        // 다른 열려있는 레이어가 있으면 닫음.
+        const visibleLayer = document.querySelectorAll('.popover-layer.is-active');
+        if (visibleLayer.length) {
+          for (let i=0, c=visibleLayer.length; i<c; i++) {
+            if (visibleLayer[i].id === id) continue;
+            visibleLayer[i].classList.remove('is-active');
+          }
+        }
+
+        if (layer.classList.contains('is-active')) {
+          layer.classList.remove('is-active');
+        } else {
+          this.calculate(_this, layer, () => {
+            layer.classList.add('popover-layer');
+            this.open(_this, layer);
+          });
+        }
+      });
+    }
+  } // constructor end
+
+  calculate(trigger, layer, callback) {
+    const rect = trigger.getBoundingClientRect();
+    let layerHeight = layer.offsetHeight;
+    let layerWidth = layer.offsetWidth;
+
+    let triggerOffset = {
+      top: trigger.offsetTop,
+      left: trigger.offsetLeft
+    }
+
+    let layerOffset = {
+      top: (() => {
+        let gutter = !this._options.overlapSelector && this._options.gutter ? this._options.gutter : 0;
+        if (window.innerHeight < rect.top + layerHeight) {
+          return triggerOffset.top - layerHeight - gutter + Number(`${this._options.overlapSelector?rect.height:0}`);
+        } else {
+          return triggerOffset.top + Number(`${this._options.overlapSelector?0:rect.height}`) + gutter;
+        }
+      })(),
+      left: (() => {
+        let left;
+        switch (trigger.getAttribute('data-alignment')) {
+          case 'right':
+          left = triggerOffset.left + rect.width - layerWidth;
+          break;
+          case 'center':
+            left = triggerOffset.left - layerWidth/2 + rect.width/2;
+            break;
+          default:
+            left = triggerOffset.left
+            break;
+        }
+        return left;
+      })()
+    }
+    console.log(layerOffset);
+    layer.style.cssText = `top: ${layerOffset.top}px; left: ${layerOffset.left}px`;
+    if (typeof callback === 'function') callback(layerOffset);
+  }
+
+  open(trigger, layer) {
+    layer.classList.add('is-active');
+    if (typeof this._options.callback === 'function') {
+      this._options.callback();
+    }
+  }
+};
+
+Popover.VERSION = '1.0.0';
+module.exports = Popover;
